fbc0ca98467c7fc3ba19b117dd95f0ff33c70f4a
Julien Ferand <hello@julienferand.dev>
2022-03-26 16:12:25 +0100

    fix no engine error

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,6 @@ import indexRouter from "@/routes/Index";
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,8 +30,7 @@ app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json(err);
 });
 
 export default app;
