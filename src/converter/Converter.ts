13bbd755a4c7bc6c9925d9dd61622d255cdd316a
Zetsuy <jeremygarciamobile@gmail.com>
2022-04-18 18:12:34 +0200

    Update convert

--- src/converter/Converter.ts
+++ src/converter/Converter.ts
@@ -1,29 +1,37 @@
 import { SensorType } from "@/models/Sensor";
 
-export function convert(rawValue: number) { 
-    var rangeT = [-20, 55];
-    var rangeBar = [950, 1150];
+export function convert(rawValue: number | boolean) { 
 
-    if(SensorType.TEMPERATURE){
-        var T = (rawValue/1023)*(rangeT[1]-rangeT[0])+(rangeT[0]);
-        return T;
-    } else if (SensorType.HUMIDITY) {
-        var Hum = (rawValue/1023)*100;
-        return Hum;
-    }
-    else if (SensorType.BARO){
-        var Baro = (rawValue/1023)*(rangeBar[1]-rangeBar[0])+(rangeBar[0]);
-        return Baro;
-    }
+    if(typeof rawValue == "number"){
+        var rangeT = [-20, 55];
+        var rangeBar = [950, 1150];
 
-}
-
-export function convertBoolean(value : boolean){
-    var Result;
-    if(value=false){
-        Result = "Inactif";
-    } else if (value=true){
-        Result = "Actif"
+        if(SensorType.TEMPERATURE){
+            var T = (rawValue/1023)*(rangeT[1]-rangeT[0])+(rangeT[0]);
+            var resultat = T.toString();
+            return resultat;
+        } else if (SensorType.HUMIDITY) {
+            var Hum = (rawValue/1023)*100;
+            var resultat = Hum.toString();
+            return resultat;
+        }
+        else if (SensorType.BARO){
+            var Baro = (rawValue/1023)*(rangeBar[1]-rangeBar[0])+(rangeBar[0]);
+            var resultat = Baro.toString();
+            return resultat;
     }
-    return Result;
+    }
+    else if (typeof rawValue == "boolean"){
+
+        var Result;
+        if(rawValue=false){
+            Result = "Inactif";
+        } else if (rawValue=true){
+            Result = "Actif"
+        }
+        return Result;
+
+    }
+    
+
 }
